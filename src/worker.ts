2ac4b736e5aed99a5842f117d4a46e7d4e16081c
pl571868110-a11y <pl571868110@gmail.com>
2026-09-15 17:03:58 +0200

    Route Shopify Poland MCP endpoint

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,6 @@
 import legacyWorker from "./index";
 import { handleGoogleRequest } from "./google";
+import { handleShopifyRequest } from "./shopify-pl";
 
 export default {
 	async fetch(
@@ -9,6 +10,10 @@ export default {
 	): Promise<Response> {
 		const googleResponse = await handleGoogleRequest(request, env, ctx);
 		if (googleResponse) return googleResponse;
+
+		const shopifyResponse = await handleShopifyRequest(request, env, ctx);
+		if (shopifyResponse) return shopifyResponse;
+
 		return legacyWorker.fetch(request, env, ctx);
 	},
 } satisfies ExportedHandler<Env>;
